4af9ba21f76c6d5e01e62f2e07e6c771dd6f3e21
yassinech26 <cherifyassinee@example.com>
2026-02-28 01:34:33 +0100

    spring boot starter

---
+++ src/main/java/com/cherifyedeshemdenebenhamed/demo/SwapTnApplication.java
@@ -0,0 +1,13 @@
+package com.cherifyedeshemdenebenhamed.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapTnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapTnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherifyedeshemdenebenhamed/demo/SwapTnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherifyedeshemdenebenhamed.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapTnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
